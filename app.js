c01ae804d1a0e6a999e529dee6b8ea9b21372c19
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2023-05-09 08:59:55 -0400

    completed

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const port = 3000
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
-let items = ["buy food", "cook food", "eat food"]
+const items = []
+const workItems = []
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-  let today = new Date();
-  
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    }
 
-    let day = today.toLocaleDateString('en-US', options)
+    let day = date.getDate();
 
-  res.render("list", { kindOfDay: day, newListItem: items });
+  res.render("list", { listTitle: day, newListItem: items });
 });
 
 app.post('/', (req, res) => {
     let item = req.body.newItem
-    items.push(item)
 
-    res.redirect('/')
+    if (req.body.list === 'Work') {
+        workItems.push(item)
+        res.redirect('/work')
+    } else {
+        items.push(item)
+        res.redirect('/')
+    }
+
+})
+
+app.get('/work', (req, res) => {
+    res.render('list', {listTitle: 'Work List', newListItem: workItems})
 })
 
 app.listen(port, () => {
